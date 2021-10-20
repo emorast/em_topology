2e8e16366cb7ed469312308f9a3229a8742b1667
emorast <egil.morast@gmail.com>
2021-10-20 07:55:11 +0200

    Added queue class

---
+++ queue.py
@@ -0,0 +1,37 @@
+
+class Node:
+
+
+    def __init__(self, id, next = None, prev = None):
+
+        self.id = id
+        self.next = None
+        self.prev = None
+
+        return
+
+
+
+class Queue:
+
+    def __init__(self):
+
+        self.__head = None
+        self.__tail = None
+        self.length = 0
+
+
+        return
+
+    def enqueue(self, toadd):
+
+
+        return
+
+    def dequeue(self, toremove):
+
+        return
+
+    def isEmpty(self):
+
+        return
